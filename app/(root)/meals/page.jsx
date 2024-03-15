c14bee80ee48f7fa2fe70c6d6d47b536ba36278d
Aakash <aakashanand1995@gmail.com>
2024-03-15 19:43:42 +0530

    added new pages

--- app/(root)/meals/page.jsx
+++ app/(root)/meals/page.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import classes from "@/app/(root)/meals/page.module.css";
+import Link from "next/link";
 
 const MealsPage = () => {
-  return <div>MealsPage</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals,created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share Your Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 };
 
 export default MealsPage;
