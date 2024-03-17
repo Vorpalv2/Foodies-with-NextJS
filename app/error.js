387e09ae41702551e22ed7f27936dbd36a26feec
Aakash <aakashanand1995@gmail.com>
2024-03-17 20:23:14 +0530

    added error and pnf errorpage

---
+++ app/error.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="error">
+      <h1>Something went wrong</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="not-found">
+      <h1>Page Not Found! Returning 404 Error</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
