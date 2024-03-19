86bc8909633a228979a0581d97d1bd12d0c1fac8
Aakash <aakashanand1995@gmail.com>
2024-03-19 09:16:32 +0530

    updated slug page

---
+++ app/(root)/meals/[slug]/page.jsx
@@ -0,0 +1,35 @@
+import classes from "@/app/(root)/meals/[slug]/mealShare.module.css";
+import React from "react";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+
+const CustomIDPage = ({ params }) => {
+  const meal = getMeal(params.slug);
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={params.slug} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal?.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+};
+
+export default CustomIDPage;
--- app/(root)/meals/backup-loading.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "@/app/(root)/meals/loading.module.css";
-
-const Loading = () => {
-  return <h2 className={classes.loading}>Loading Meals...</h2>;
-};
-
-export default Loading;
